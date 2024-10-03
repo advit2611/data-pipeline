67b893724d430dfaf0b22acfecf57d652c58360a
advit2611 <advit214@gmail.com>
2024-10-02 18:38:59 -0600

    Added a surrogate key to the view

--- models/staging/stg_tpch_lineitem.sql
+++
@@ -1,7 +0,0 @@
-select
-    o_orderkey as orderkey,
-    o_custkey as customer_key,
-    o_orderstatus as status_code,
-    o_totalprice as total_price
-from
-    {{ source('tpch', 'orders')}}
--- models/staging/stg_tpch_orders.sql
+++ models/staging/stg_tpch_orders.sql
@@ -1,5 +1,5 @@
 select
-    o_orderkey as orderkey,
+    o_orderkey as order_key,
     o_custkey as customer_key,
     o_orderstatus as status_code,
     o_totalprice as total_price
